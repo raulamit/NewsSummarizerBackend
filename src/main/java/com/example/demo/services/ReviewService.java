4ddeb9278edce792ee5cacb9c2959e83b1769379
Amit Raul <raulamit@ccs.neu.edu>
2018-06-25 14:05:40 -0400

    reviews completed

--- src/main/java/com/example/demo/services/ReviewService.java
+++ src/main/java/com/example/demo/services/ReviewService.java
@@ -2,6 +2,10 @@ package com.example.demo.services;
 
 import java.util.Optional;
 
+import com.example.demo.models.NewsSummary;
+import com.example.demo.models.Reader;
+import com.example.demo.models.User;
+import com.example.demo.repositories.NewsSummaryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,18 +19,39 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.models.Review;
 import com.example.demo.repositories.ReviewRepository;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
-@CrossOrigin (origins = "*", maxAge = 3600)
+@CrossOrigin (origins = "http://localhost:4200", maxAge = 3600, allowCredentials= "true")
 public class ReviewService {
 	
 	@Autowired
 	ReviewRepository reviewRepository;
+
+	@Autowired
+	NewsSummaryRepository newsSummaryRepository;
 	
 	//CREATE NEW Review
 	@PostMapping("/api/review")
 	public Review createReview(
 			@RequestBody Review review) {
 		return reviewRepository.save(review);
+	}
+
+	@PostMapping("/api/newssummary/{summaryId}/review")
+	public Review createReview(
+			HttpSession session,
+			@PathVariable("summaryId") int summaryId,
+			@RequestBody Review newReview) {
+		Optional<NewsSummary> data = newsSummaryRepository.findById(summaryId);
+		User user = (User) session.getAttribute("currentUser");
+		if(data.isPresent()) {
+			NewsSummary newsSummary = data.get();
+			newReview.setSummary(newsSummary);
+			newReview.setReader(new Reader(user));
+			return reviewRepository.save(newReview);
+		}
+		return null;
 	}
 	
 	//FIND Review BY Review ID
@@ -58,8 +83,7 @@ public class ReviewService {
 	@DeleteMapping("/api/review/{reviewId}")
 	public void deleteReview(@PathVariable("reviewId") int reviewId) {
 		reviewRepository.deleteById(reviewId);
-		
-	}	
+	}
 
 
 }
