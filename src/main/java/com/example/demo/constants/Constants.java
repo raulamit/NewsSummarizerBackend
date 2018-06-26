88e1ffb76c4c1c09133caf30bbfde1d14b4776b4
Amit Raul <raulamit@ccs.neu.edu>
2018-06-26 03:41:53 -0400

    constants file changed

--- src/main/java/com/example/demo/constants/Constants.java
+++ src/main/java/com/example/demo/constants/Constants.java
@@ -2,5 +2,5 @@ package com.example.demo.constants;
 
 public class Constants {
     public static final String FRONT_END_SERVER = "http://localhost:4200";
-//    public static String FRONT_END_SERVER = "https://newssummarizer-webdev2018-ng.herokuapp.com/";
+//    public static String FRONT_END_SERVER = "https://newssummarizer-webdev2018-ng.herokuapp.com";
 }
